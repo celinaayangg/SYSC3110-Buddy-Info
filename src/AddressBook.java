b12158b985a896950e788ef778e3de91c757c6ca
Celina Yang <celinayang18@hotmail.com>
2025-09-14 17:06:08 -0400

    Added default constructor Implemented addBuddy and removeBuddy

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -5,20 +5,32 @@ public class AddressBook {
     // instance variable
     private ArrayList<BuddyInfo> buddyCollection;
 
+    // default constructor
+    public AddressBook() {
+        buddyCollection = new ArrayList<>();
+    }
+
     public void addBuddy(BuddyInfo buddy) {
         if (buddy != null) {
             buddyCollection.add(buddy);
+            System.out.println("Added Buddy");
         }
     }
 
     public void removeBuddy(BuddyInfo buddy) {
         if (buddy != null) {
             buddyCollection.remove(buddy);
+            System.out.println("Removed Buddy");
         }
     }
 
     public static void main(String[] args) {
         System.out.println("Address Book");
-    }
 
+        BuddyInfo buddy = new BuddyInfo("Marcus", "Ottawa", "647-282-8283");
+        AddressBook addressBook = new AddressBook();
+
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
+    }
 }
